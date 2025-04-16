9673c25cca07c247234239229059174e3c61013c
Kathirvel E <eshakathirvel@gmail.com>
2025-04-17 05:08:21 +0530

    Add files via upload

---
+++ CH-1.sql
@@ -0,0 +1,11 @@
+/*
+SITUATION 
+WE want to send out a promotional email to our 
+existing customers
+CHALLENGE
+Use a SELECT statement to grsb the firast and last names 
+of every customer and their email address*/
+SELECT first_name,last_name,email
+FROM customer;
+
+
