49bcd159da56ce68939724086b7f7fd433f0b912
Kathirvel E <eshakathirvel@gmail.com>
2025-04-17 05:18:28 +0530

    Add files via upload

---
+++ CH-2.sql
@@ -0,0 +1,11 @@
+/*
+STIUATION
+An australian visitor isn't familiar with movie 
+ratings
+We want to know the types of ratings
+CHALLENGE
+Use DISTINCT to retrieve the distinct rating types our
+films could have in our database
+*/
+SELECT DISTINCT (rating)
+FROM film
