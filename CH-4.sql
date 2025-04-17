5261331edfc33f1c29c959f8822bec009c4305dd
Kathirvel E <eshakathirvel@gmail.com>
2025-04-17 20:27:38 +0530

    Add files via upload

---
+++ CH-4.sql
@@ -0,0 +1,7 @@
+/* A customer wants to know what the movie "Outlaw Hanky"
+is about.could you give them the description for the movie
+"Outlaw Hanky"?*/
+
+SELECT description
+FROM film
+WHERE title='Outlaw Hanky';
